0b61a21a082c66b4c99f102d760964c9db380676
Dewdrops <v_v_4474@126.com>
2013-12-25 19:56:10 +0800

    add .inputrc and .wgetrc

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set +e
 
 scriptdir=$(dirname $(readlink -e $0))
 
-for f in .vimrc .vimperator .vimperatorrc .bashrc .zshrc .gitconfig
+for f in .vimrc .vimperator .vimperatorrc .bashrc .zshrc .gitconfig .wgetrc .inputrc
 do
     CMD="rm -rf $HOME/$f"
     echo $CMD
